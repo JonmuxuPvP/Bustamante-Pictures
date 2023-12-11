c2961435f00733e4ab30cd85dd85be86ba9b6240
Jonmuxu <Jonmuxupvp@gmail.com>
2023-12-11 23:39:19 +0100

    Added script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("button");
+const image = document.getElementById("image");
+
+function randomPicture() {
+    const picture = random();
+    image.src = `./assets/${picture}.jpg`;
+}
+
+function random() {
+    const maximum = 140;
+    return Math.floor((Math.random() * maximum) + 1);
+}
+
+button.addEventListener("click", (event) => {
+    randomPicture();
+});
