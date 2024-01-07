acec7895a0876d3b0d13903b0a19dfbeced790b1
Jonmuxu <Jonmuxupvp@gmail.com>
2024-01-07 22:05:32 +0100

    Added previousPicture()

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const button = document.getElementById("button");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
 const image = document.getElementById("image");
+let seenPictures = [];
 
 function randomPicture() {
     const picture = random();
     image.src = `./assets/${picture}.jpg`;
+
+    seenPictures.push(picture);
+
+    console.log(seenPictures);
+}
+
+function previousPicture() {
+    if (seenPictures.length > 1) {
+        seenPictures.pop();
+        image.src = `./assets/${seenPictures[seenPictures.length - 1]}.jpg`;
+        console.log(seenPictures);
+    }
 }
 
 function random() {
@@ -11,6 +25,10 @@ function random() {
     return Math.floor((Math.random() * maximum) + 1);
 }
 
-button.addEventListener("click", (event) => {
+next.addEventListener("click", (event) => {
     randomPicture();
+});
+
+previous.addEventListener("click", (event) => {
+    previousPicture();
 });
